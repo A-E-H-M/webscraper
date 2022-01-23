98b36ed751d6f6a68c624c75ec9fad536ab43cb7
Alexandria Hernandez Mann <bombus@Alexandrias-MacBook-Air-2.local>
2022-01-23 13:21:32 -0600

    Add beautiful soup module

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 
 response_object = requests.get('https://www.nwf.org/Educational-Resources/Wildlife-Guide/Invertebrates/Bees')
 
-print(response_object.text)
+soup_object = BeautifulSoup(response_object.text, 'html.parser')
+
+divs = soup_object.find_all('div', {'class': 'bordered-container'})
+
+for div in divs:
+    print(div)
+
